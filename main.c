fe29304f5a6376c5da6f7723be76f9557ecc1f96
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2024-10-02 23:35:20 +0000

    adicionado projeto final do jgo de perguntas

--- main.c
+++ main.c
@@ -265,3 +265,253 @@ void ledVermelho(){
 // fim das funções de sons e leds 
 //////////////////////////////////////////////////////////////////////////////
 
+
+///////////////////////////////////////////////////////////////////////////
+// Função para a fase de perguntas
+void jogoDasPerguntas() {
+
+    //limpa o lcd e exibe a mensagem
+    lcd_1.clear();
+    lcd_1.print("Perguntinhas");
+    delay(2500);
+
+    //variavel para vezes que jogador pode pular
+    int qtdePulos = 1; 
+
+
+    int qtdePerguntas[5];
+
+    for (int i = 0; i < 5; i++) {
+
+        int indPergunta;
+        bool repetida;
+
+
+        do {
+            repetida = false;
+            //gera um numero aleatoria entre 0 e 7
+            indPergunta = random(8); 
+
+            //percorre a lista de perguntas com base no indice que cair
+            for (int j = 0; j < i; j++) {
+                //verifica se a pergunta esta sendo repetida
+                if (qtdePerguntas[j] == indPergunta) {
+                    repetida = true;
+                    break;
+                }
+            }
+        } while (repetida);
+        qtdePerguntas[i] = indPergunta;
+
+
+        //exibe no lcd a pergunta a ser feita por 3 segundos
+        lcd_1.clear();
+        scrollTexto(perguntas[indPergunta], 300);
+        Serial.print("Pergunta: ");
+        Serial.println(perguntas[indPergunta]);
+        delay(3000);
+        lcd_1.clear();
+
+        // Aguarda resposta 
+        bool respPergunta = false;
+        bool respJogador = false;
+
+        //conta o tempo para a resposta do jogador
+        for (int tempo = 9; tempo > -1; tempo--) { 
+        	
+            //mostra o contador do tempo    
+        	lcd_1.setCursor(5,0);
+            lcd_1.print("tempo: ");
+          	lcd_1.setCursor(12,0);
+            lcd_1.print(tempo);
+            lcd_1.setCursor(3,1);
+            lcd_1.print("Sim");
+            lcd_1.setCursor(10,1);
+            lcd_1.print("Nao");
+
+            
+            if (tempo <= 4) {
+                tone(buzzer, 1000); // Emite som de contagem regressiva
+            }
+
+            if (digitalRead(botaoInicia) == HIGH) {
+                return; // Sai do jogo
+            }
+            //verifica a resposta do jogador se for sim
+            if (digitalRead(botaoVerde) == HIGH) {
+                respJogador = true;
+                respPergunta = true;
+                lcd_1.setCursor(2,1);
+                lcd_1.print("*");
+                noTone(buzzer);
+                delay(200);
+                
+                
+            }
+            //verifica se a resposta do jogador foi não 
+            else if (digitalRead(botaovermelho) == HIGH) {
+                respJogador = false;
+                respPergunta = true;
+                lcd_1.setCursor(9,1);
+                lcd_1.print("*");
+                noTone(buzzer);
+                delay(200);
+                
+            }
+            delay(1000); // Espera 1 segundo
+        }
+        noTone(buzzer); // Para o som
+
+
+        // Se o tempo acabar pula a questão e subtrai as vezes possiveis de pulo
+        if (!respPergunta) {
+            if (qtdePulos > 0) {
+              
+				String pulouPerg = "Pulou a pergunta";
+                qtdePulos--;
+              	lcd_1.clear();
+                scrollTexto(pulouPerg,350);
+                tone(buzzer, 500, 500);
+                delay(1000);
+                continue; // Vai para a próxima pergunta
+
+            } 
+            //caso não tenha mais pulos para o jogo
+            else {
+              
+              	String pulouPerg = "Sem pulos restantes";
+                lcd_1.clear();
+                scrollTexto(pulouPerg,350);
+                tone(buzzer, 200, 500);
+                delay(1000);
+                return; // Sai do jogo
+            }
+        }
+
+        // Verifica resposta do jogador se esta correta ou não
+        if (respJogador == respostas[indPergunta]) {
+            lcd_1.clear();
+            lcd_1.print("Correto!");
+            somV(); // Emite som de vitória
+            delay(500);
+        } else {
+            lcd_1.clear();
+            lcd_1.print("Errado!");
+            somD(); // Emite som de derrota
+            delay(1000);
+            return; // Sai do jogo
+        }
+    }
+
+    // Pergunta final
+    ultimaPergunta();
+}
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////
+//executa a pergunta final do jogo
+void ultimaPergunta() {
+
+    //exibe no lcd a mensagem de ultima pergunta
+    lcd_1.clear();
+    lcd_1.print("Pergunta Final");
+    delay(2000);
+
+
+    //ultima pergunta e respostas
+    String pergunta = "Arduino e um microcontrolador?";
+    bool respUltPergunta = true;
+
+    //exibe a perfunta final 
+    lcd_1.clear();
+    scrollTexto(pergunta, 300);
+    Serial.print("Pergunta Final: ");
+    Serial.println(pergunta);
+    delay(3000);
+    lcd_1.clear();
+    
+    bool respPerguntaUlt = false;
+    bool respJogadorUlt = false;
+
+    for (int tempo = 9; tempo > -1; tempo--) { 
+        	
+        //exibe o tempo e a resposta do jogador
+        lcd_1.setCursor(5,0);
+        lcd_1.print("tempo: ");
+        lcd_1.setCursor(12,0);
+        lcd_1.print(tempo);
+        lcd_1.setCursor(3,1);
+        lcd_1.print("Sim");
+        lcd_1.setCursor(10,1);
+        lcd_1.print("Nao");
+        
+
+        //contagem regressiva
+        if (tempo <= 4) {
+            tone(buzzer, 1000); 
+        }
+        //volta o jogo
+        if (digitalRead(botaoInicia) == HIGH) {
+            return; 
+        }
+
+        //verifica se a resposta do jogador foi sim
+        if (digitalRead(botaoVerde) == HIGH) {
+            respJogadorUlt = true;
+            respPerguntaUlt = true;
+            lcd_1.setCursor(2,1);
+            lcd_1.print("*");
+            noTone(buzzer);
+            
+            
+        } 
+        //verifica se a resposta do jogador foi não
+        else if (digitalRead(botaovermelho) == HIGH) {
+            respJogadorUlt = false;
+            respPerguntaUlt = true;
+            lcd_1.setCursor(9,1);
+            lcd_1.print("*");
+            noTone(buzzer);
+            
+        }
+        delay(1000);
+    }
+
+    //caso jogador não responda exibe o tempo esgotado e finaliza o jogo
+    if (!respPerguntaUlt) {
+        lcd_1.clear();
+        lcd_1.print("Tempo esgotado");  
+        tone(buzzer, 200, 500);
+        Serial.println("Tempo esgotado na pergunta final.");
+        delay(1000);
+        return; // Sai do jogo
+    }
+
+    //emite a mensagem de vitoria se responder tudo certo
+    if (respJogadorUlt == respUltPergunta) {
+        lcd_1.clear();
+        lcd_1.print("Voce Venceu!");
+        somV(); //Emite o som de vitória do jogo
+        Serial.println("Jogador Venceu!");
+    } 
+    //exibe a mensagem de derrota caso erre
+    else {
+        lcd_1.clear();
+        lcd_1.print("Errado!");
+        somD(); //Emite o som de derrota do jogo
+        Serial.println("Resposta Incorreta na Pergunta Final!");
+        delay(1000);
+    }
+}
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////
+//inicia o jogo
+void loop() {
+    if (digitalRead(botaoInicia) == HIGH) {
+        jgMemoria();
+    }
+}
+///////////////////////////////////////////////////////////////////////////
